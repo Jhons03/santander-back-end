25e8be0546e30b2cfd0e84a7abf71a0d1f662f44
Edson Marco <edin2008@yahoo.com.br>
2024-06-05 20:43:23 -0300

    Initial commit

---
+++ src/main/java/mi/dio/santander_back_end/SantanderBackEndApplication.java
@@ -0,0 +1,13 @@
+package mi.dio.santander_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mi/dio/santander_back_end/SantanderBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package mi.dio.santander_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
